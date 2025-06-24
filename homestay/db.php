319dcc842eca909dcce41993be0cec393a030506
KencanaP <141292547+KencanaP@users.noreply.github.com>
2025-06-25 00:38:50 +0800

    Update db.php

--- homestay/db.php
+++ homestay/db.php
@@ -1,2 +1,14 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "homestay") or die(mysqli_connect_error());
+$servername = "localhost";
+$username = "root"; // Change if your MySQL user is different
+$password = "";     // Change if your MySQL password is set
+$dbname = "homestay";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
